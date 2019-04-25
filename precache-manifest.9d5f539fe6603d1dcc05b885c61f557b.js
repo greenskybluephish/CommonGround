b58f04a1ad306d88da48cc5a3e48739ba4325792
greenskybluephish <greenskybluephish@gmail.com>
2019-04-25 18:33:41 -0500

    Updates

---
+++ precache-manifest.9d5f539fe6603d1dcc05b885c61f557b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/tests/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "131da320403d07f4fc97",
+    "url": "/tests/static/css/main.deedf536.chunk.css"
+  },
+  {
+    "revision": "8a50748c4fc2a058fe70",
+    "url": "/tests/static/js/runtime~main.227e9f38.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/tests/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "410817f1d68ebebf8c17",
+    "url": "/tests/static/js/2.a25ef84c.chunk.js"
+  },
+  {
+    "revision": "131da320403d07f4fc97",
+    "url": "/tests/static/js/main.8917df53.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/tests/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/tests/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/tests/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "410817f1d68ebebf8c17",
+    "url": "/tests/static/css/2.2c91e9f2.chunk.css"
+  },
+  {
+    "revision": "0ca9a5343618069f70b4a7207987fb5c",
+    "url": "/tests/index.html"
+  }
+];
