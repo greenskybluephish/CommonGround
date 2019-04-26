945f80767d50cc6fe305efd67c32c65fc79e8590
greenskybluephish <greenskybluephish@gmail.com>
2019-04-25 20:42:35 -0500

    /

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -144,7 +144,7 @@ class ApplicationViews extends Component {
             if (!this.isAuthenticated()) {
               return <Redirect to="/login" />;
             } else {
-              return <Weather {...props} userId={this.state.userId} secondUserAdd={this.secondUserAdd}/>;
+              return <Weather userId={this.state.userId} secondUserAdd={this.secondUserAdd}/>;
             }
           }}
         />
